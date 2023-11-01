94375a1413939cce371e14ef61b8f3efb372877e
tommyBjorkman <tommy.bjorkman@utb.ecutbildning.se>
2023-11-01 06:03:44 +0100

    MailService completed

--- Services/MailService.cs
+++ Services/MailService.cs
@@ -1,18 +1,53 @@
-﻿namespace Crito.Services;
+﻿using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+namespace Crito.Services;
 
 public class MailService : IDisposable
 {
-    private bool _disposed;
+    private string _from;
+    private string _smtp;
+    private int _port;
+    private string _username;
+    private string _password;
+    private MailKit.Net.Smtp.SmtpClient _client;
+
+    public MailService(string from, string smtp, int port, string username, string password)
+    {
+        _form = from;
+        _smtp = smtp;
+        _port = port;
+        _username = username;
+        _password = password;
+        _client = new MailKit.Net.Smtp.SmtpClient();
+    }
+
+    private async Task SendAsync(string to, string subject, string body)
+    {
+        try
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_from));
+            email.To.Add(MailboxAddress.Parse(to));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+            await _client.ConnectAsync(_smtp, _port, SecureSocketOptions.StartTls);
+            await _client.AuthenticateAsync(_username, _password);
+
+            var result = await _client.SendAsync(email);
+        }
+        catch { }
+    }
     public void Dispose()
     {
         Dispose(true);
         GC.SuppressFinalize(this);
     }
-    protected virtual void Dispose(bool disposing)
+    protected virtual async void Dispose(bool disposing)
     {
         if (disposing)
-        {
-            _disposed = true;
-        }
+            await _client.DisconnectAsync(true);
     }
 }

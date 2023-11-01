d019ee85e81a7f5788628d86b410bf58215108ba
tommyBjorkman <tommy.bjorkman@utb.ecutbildning.se>
2023-11-01 05:36:52 +0100

    added mailservice and contactform in vs

---
+++ Services/MailService.cs
@@ -0,0 +1,18 @@
+﻿namespace Crito.Services;
+
+public class MailService : IDisposable
+{
+    private bool _disposed;
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _disposed = true;
+        }
+    }
+}
